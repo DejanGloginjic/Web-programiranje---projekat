9f50c8fd7fde499ae905f2c090fd41ba3c2eaaa4
Dejan Gloginjic <deki555@hotmail.com>
2022-06-17 14:16:55 +0200

    updated model

--- WebShopREST-reseno/WebShopREST/src/beans/BuyerType.java
+++ WebShopREST-reseno/WebShopREST/src/beans/BuyerType.java
@@ -4,10 +4,55 @@ import beans.Enums.BuyerTypeEnum;
 
 public class BuyerType {
 	
+	private int id;
 	private BuyerTypeEnum buyerType;
 	private double discount;
 	private int points;
 	
-	
+	public BuyerType(int id, BuyerTypeEnum buyerType, double discount, int points) {
+		super();
+		this.id = id;
+		this.buyerType = buyerType;
+		this.discount = discount;
+		this.points = points;
+	}
 
+	public BuyerType() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public BuyerTypeEnum getBuyerType() {
+		return buyerType;
+	}
+
+	public void setBuyerType(BuyerTypeEnum buyerType) {
+		this.buyerType = buyerType;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	
+	
+	
 }
