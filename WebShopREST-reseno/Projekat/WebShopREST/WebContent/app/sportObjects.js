e792859eacee26329c43e05c9e22f2a2c8405638
Dejan Gloginjic <deki555@hotmail.com>
2022-07-06 18:19:31 +0200

    implemented combine object search

--- WebShopREST-reseno/Projekat/WebShopREST/WebContent/app/sportObjects.js
+++ WebShopREST-reseno/Projekat/WebShopREST/WebContent/app/sportObjects.js
@@ -2,19 +2,35 @@ var app = new Vue({
 	el: '#sportObjects',
 	data: {
 		sportObjects: null,
-		filter: '',
-		criterium: '',
+		searchedSportObjects: [],
+		searchName: "",
+		searchType: "",
+		searchCity: "",
+		searchMinGrade: "",
+		searchMaxGrade: "",
 		loggedUser: {}
 	},
 	mounted() {
 		axios.get('rest/sportobjects')
-			.then(response => (this.sportObjects = response.data))
+			.then(response => (
+				this.sportObjects = response.data,
+				this.searchedSportObjects = response.data
+				))
 		axios.get('rest/currentUser').then(response=>(this.loggedUser = response.data))
 	},
 	methods: {
-		searchObject: function(){
+		/*searchObject: function(){
 			axios.get('rest/sportobjects/search', { params: { searchValue: this.filter, criterion: this.criterium} } )
 				.then(response => (this.sportObjects = response.data))
+		},*/
+		searchObjects: function(){
+			this.searchedSportObjects = []
+			for(let object of this.sportObjects){
+				if(object.objectName.toLowerCase().includes(this.searchName.toLowerCase()) && object.objectType.includes(this.searchType) 
+					&& object.location.place.toLowerCase().includes(this.searchCity.toLowerCase())){
+					this.searchedSportObjects.push(object)
+				}
+			}
 		},
 		Selected: function(sp) {
 			axios.post('rest/sportobjects/setSelected',  {id: sp.id}).then(response=>{
