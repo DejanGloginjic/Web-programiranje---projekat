ef5c5941092128f02a506f2e461d84b30a180df5
Dejana Tesanovic <dejana.tesanovic5@gmail.com>
2022-07-10 23:24:10 +0200

    Added sort of all users

--- WebShopREST-reseno/Projekat/WebShopREST/WebContent/app/registeredUsers.js
+++ WebShopREST-reseno/Projekat/WebShopREST/WebContent/app/registeredUsers.js
@@ -22,11 +22,51 @@ var app = new Vue({
 	methods: {
 		findUsers: function() {
 			this.searchedUser = []
+
 			for(let user of this.users){
-				if(user.name.includes(this.searchName)){
-					this.searchedUser.push(user)
+				if(user.name.toLowerCase().includes(this.searchName.toLowerCase()) && user.surname.toLowerCase().includes(this.searchSurname.toLowerCase())
+					&& user.username.toLowerCase().includes(this.searchUsername.toLowerCase()))
+					{
+						this.searchedUser.push(user)
 				}
 			}
+		},
+		Sort: function(n) {
+			//function found on site: https://www.w3schools.com/howto/howto_js_sort_table.asp
+			  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+			  table = document.getElementById("tabela");
+			  switching = true;
+			  dir = "asc";
+			  while (switching) {
+			    switching = false;
+			    rows = table.rows;
+			    for (i = 1; i < (rows.length - 1); i++) {
+			      shouldSwitch = false;
+			      x = rows[i].getElementsByTagName("TD")[n];
+			      y = rows[i + 1].getElementsByTagName("TD")[n];
+			      if (dir == "asc") {
+			        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+			          shouldSwitch = true;
+			          break;
+			        }
+			      } else if (dir == "desc") {
+			        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+			          shouldSwitch = true;
+			          break;
+			        }
+			      }
+			    }
+			    if (shouldSwitch) {
+			      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			      switching = true;
+			      switchcount ++;
+			    } else {
+			      if (switchcount == 0 && dir == "asc") {
+			        dir = "desc";
+			        switching = true;
+			      }
+			    }
+			  }
 		}
 
 
